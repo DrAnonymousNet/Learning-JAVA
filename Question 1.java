0db2ae7ec0e29b9ca3c3618d2079b79775ce367e
Mustapha Ahmad <64500446+DrAnonymousNet@users.noreply.github.com>
2020-07-27 01:42:57 +0100

    Create Question 1.java

---
+++ Question 1.java
@@ -0,0 +1,11 @@
+//1. Write a program that declares four integer variables equal to 2, 4, 5, and 10 and prints out their average (which should be 5.25)
+public class Main {
+	public static void main(String[] args) {
+	
+	int x = 2, y = 4 , z = 5 ,w = 10;
+	
+	double Average = (x + y + z + w)/4.0;
+	
+	System.out.print(Average);
+	}
+}
